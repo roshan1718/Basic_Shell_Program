937b71c2ec9aa1e8a02d8a0222793888e9b9e858
roshan1718 <shinderoshan.2902@gmail.com>
2020-03-12 19:11:42 +0530

    ADDED code to find weekDay using case

---
+++ weekDayCase.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -x
+read -p "Enter the number::" number
+
+case $number in
+		1)
+			printf "MONDAY" ;;
+		2)
+      	printf "THUSDAY" ;;
+    	3)
+     		printf "WEDNEDAY" ;;
+    	4)
+      	printf "THURSDAY" ;;
+    	5)
+      	printf "FRIDAY" ;;
+    	6)
+      	printf "SATURDAY" ;;
+    	7)
+      	printf "SUNDAY" ;;
+		*)
+			printf "Invalid Input" ;;
+esac
