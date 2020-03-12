ad8efd9c359a4071be4cceb834a1cd9c9dc06408
roshan1718 <shinderoshan.2902@gmail.com>
2020-03-12 15:57:49 +0530

    ADDED code to print true if day and month is between march 20 and june 20

---
+++ dayMonth.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash -x
+
+month=$1
+date=$2
+
+if (( $month < 3 || $month > 6 || $date > 31 || $date < 1 ))
+then
+	echo "Invalid  :: $month "
+else
+
+	if (( $month >= 3 && $month <= 6 )) 
+	then
+
+		if (( $month == 3 && $date >= 20 )) 
+		then
+		echo "valid Date"
+ 		elif(( $month == 6 && $date <= 20 ))
+ 		then
+		echo "valid Date"
+		elif(( $month == 4 && $date <= 30 || $month == 5 && $date <= 31 ))
+		then
+		echo "Valid Date"
+		else
+		echo "Invalid Date"
+		fi
+	else
+	echo " Invalid Date"
+	fi
+	
+fi
+
