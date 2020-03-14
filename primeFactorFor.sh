114d1450a917f31d72de9b370176e1613a0e099c
roshan1718 <shinderoshan.2902@gmail.com>
2020-03-14 10:41:16 +0530

    ADDED code to computer factors using prime factorization method

--- primeFactorFor.sh
+++ primeFactorFor.sh
@@ -5,21 +5,14 @@
 
 read -p "Enter Number To Find Factors " number
 
-for (( index=1; index<=$number; index++ ))
+for (( i=2; i<=$number; i++ ))
 do
-	if [[ $(($number%$index)) -eq 0 ]]
-	then
-			count=0
-			for (( j=1; j<=$index; j++ ))
-			do
-				if [[ $(( $index% $j )) -eq 0 ]]
-				then
-					count=$(($count+1))
-				fi
-			done
-			if [[ $count -eq 2 ]]
-			then
-				echo $index
-			fi
-	fi
+	for (( j=$i;($number%$j)==0; j=$i))
+	do
+		if [[ $(( $number%$j )) -eq 0 ]]
+		then
+			echo $i
+		fi
+	number=$(($number/$j))
+	done
 done
