0a354b68b11089457dca0af27f32380e822c1ceb
roshan1718 <roshanshinde1717@gmail.com>
2020-03-17 09:23:24 +0530

    ADDED code to print number of people born in same month

--- bornInSameMonth.sh
+++ bornInSameMonth.sh
@@ -15,41 +15,40 @@ for (( month=1; month<=12; month++ ))
 do
 	case $month in
 		"1")
-			echo " Birthday in same month [ Jan ]:: ${birthDay[month]}"
-			;;
+			echo " Birthday in same month [ Jan ]:: ${birthDay[month]}" ;;
+		
 		"2")
-			echo " Birthday in same month [ Feb ]:: ${birthDay[month]}"
-			;;
+			echo " Birthday in same month [ Feb ]:: ${birthDay[month]}" ;;
+
 		"3")
-			echo " Birthday in same month [ Mar ]:: ${birthDay[month]}"
-         ;;
+			echo " Birthday in same month [ Mar ]:: ${birthDay[month]}" ;;
+
 		"4")
-			echo " Birthday in same month [ Apr ]:: ${birthDay[month]}"
-         ;;
+			echo " Birthday in same month [ Apr ]:: ${birthDay[month]}" ;;
+
 		"5")
-			echo " Birthday in same month [ May ]:: ${birthDay[month]}"
-         ;;
+			echo " Birthday in same month [ May ]:: ${birthDay[month]}" ;;
+
 		"6")
-			echo " Birthday in same month [ Jun ]:: ${birthDay[month]}"
-         ;;
+			echo " Birthday in same month [ Jun ]:: ${birthDay[month]}" ;;
+
 		"7")
-			echo " Birthday in same month [ Jul ]:: ${birthDay[month]}"
-         ;;
+			echo " Birthday in same month [ Jul ]:: ${birthDay[month]}" ;;
+
 		"8")
-			echo " Birthday in same month [ Aug ]:: ${birthDay[month]}"
-         ;;
+			echo " Birthday in same month [ Aug ]:: ${birthDay[month]}" ;;
+
 		"9")
-			echo " Birthday in same month [ Sep ]:: ${birthDay[month]}"
-         ;;
-      "10")
-			echo " Birthday in same month [ Oct ]:: ${birthDay[month]}"
-         ;;
-      "11")
-			echo " Birthday in same month [ Nov ]:: ${birthDay[month]}"
-         ;;
-      *)
-			echo " Birthday in same month [ Dec ]:: ${birthDay[month]}"
-        ;;
+			echo " Birthday in same month [ Sep ]:: ${birthDay[month]}" ;;
+
+		"10")
+			echo " Birthday in same month [ Oct ]:: ${birthDay[month]}" ;;
+
+		"11")
+			echo " Birthday in same month [ Nov ]:: ${birthDay[month]}" ;;
+
+		*)
+			echo " Birthday in same month [ Dec ]:: ${birthDay[month]}" ;;
 	esac
 done
 
